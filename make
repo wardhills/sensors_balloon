d053d95808570844b6ddd8c8e6e3a2730a8fff92
Ward Hills <mail@wardhills.net>
2019-09-17 14:41:41 +0100

    First commit

---
+++ make
@@ -0,0 +1,3 @@
+venv:
+	python -m venv sensors
+	pip install -r requirements-freeze.txt
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION="0.1.0.dev0"
+
+setup(
+    name="hello",
+    version=VERSION,
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=["RPI.GPIO", "adafruit-blinka", "adafruit-circuitpython-sht31d", "adafruit-circuitpython-as726x", "adafruit-circuitpython-fxos8700", "adafruit-circuitpython-fxas21002c"]
+    )
