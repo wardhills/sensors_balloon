ed99e4573b33812eba5b16fa07ef236bac7a87a3
Ward Hills <ward@wardhills.net>
2019-09-17 16:05:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 VERSION="0.1.0.dev0"
 
 setup(
-    name="hello",
+    name="Sensors-Balloon",
     version=VERSION,
     packages=find_packages("src"),
     package_dir={'': 'src'},
