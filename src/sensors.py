d21fe5fb2a18ba3b8f7e6ad79c524b26966f72a5
Ward Hills <mail@wardhills.net>
2019-09-21 11:30:29 +0100

    formatted output and sedn data to data folder addd that folder to .gitgnore

--- src/sensors.py
+++ src/sensors.py
@@ -87,6 +87,10 @@ def read_gyroscope(sensor):
     print('Gyroscope (radians/s): ({0:0.3f},  {1:0.3f},  {2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
     # Delay for a second.
 
+    gyro_x = round(gyro_x,4)
+    gyro_y = round(gyro_y,4)
+    gyro_z = round(gyro_z,4)
+
 
     return gyro_x, gyro_y, gyro_z
 
@@ -115,6 +119,13 @@ def read_accelerometer(sensor):
     print('Acceleration (m/s^2): ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(accel_x, accel_y, accel_z))
     print('Magnetometer (uTesla): ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(mag_x, mag_y, mag_z))
 
+    accel_x = round(accel_x,2)
+    accel_y = round(accel_y,2)
+    accel_z = round(accel_z,2)
+    mag_x   = round(accel_x,3)
+    mag_y   = round(accel_y,3)
+    mag_z   = round(accel_z,3)
+
     return accel_x, accel_y, accel_z, mag_x, mag_y, mag_z
 
 
@@ -123,6 +134,9 @@ def read_temphum(sensor):
     print("Humidity: %0.1f %%" % sensor.relative_humidity)
     temp = sensor.temperature
     hum = sensor.relative_humidity
+
+    temp = round(temp,1)
+    hum  = round(hum,1)
 
     # every 10 passes turn on the heater for 1 second
 
@@ -138,7 +152,7 @@ def read_temphum(sensor):
 
 def write_file(data, fname ,headings, path='./'):
     now = datetime.datetime.now()
-    today = now.strftime("%Y%m%d%H%M")
+    today = now.strftime("%Y%m%d")
 
     # TODO make method for constructing the full path with file type exention
     full_name = fname+today+'.csv'
@@ -169,6 +183,8 @@ if __name__ == '__main__':
     headings = []
     data_values = []
     delay = 10
+    data_path = '../data/'
+    first_pass = True
     
     try:
         light = Adafruit_AS726x(i2c)
@@ -197,41 +213,45 @@ if __name__ == '__main__':
     print(sensors)
 
     while True:
-        
 
         if 'accelerometer' in sensors:
              data_values.append(read_accelerometer(accelerometer))
-             headings.append(('accel_x', 'accel_y', 'accel_z', 'mag_x', 'mag_y', 'mag_z'))
-             
+             if first_pass == True:
+                 headings.append(('accel_x', 'accel_y', 'accel_z', 'mag_x', 'mag_y', 'mag_z'))
+
         if 'gyroscope' in sensors:
              data_values.append(read_gyroscope(gyroscope))
-             headings.append(('gyro_x', 'gyro_y', 'gyro_z'))
-             
+             if first_pass == True: 
+                 headings.append(('gyro_x', 'gyro_y', 'gyro_z'))
+
         if 'light' in sensors:
              data_values.append(read_light(light))
-             headings.append(('v','b','g','y','o','r'))
+             if first_pass == True:
+                 headings.append(('v','b','g','y','o','r'))
 
         if 'temphum' in sensors:
              data_values.append(read_temphum(temphum))
-             headings.append(('t','h'))
+             if first_pass == True:
+                 headings.append(('t','h'))
 
         #print(data_values)
         #print(headings)
         data_list= list(chain.from_iterable(data_values))
         headings_list= list(chain.from_iterable(headings))
-        
-        data_list.append(str(datetime.datetime.now()))
+
+        data_list.append(datetime.datetime.now().replace(microsecond=0).isoformat())
         headings_list.append('time')
         print(headings_list)
         print(data_list)
-        
+
 
         #todo  clean up the data by converting the tuples to list and croping the floats
-        write_file(data_list,"flt_data",headings_list)
+        write_file(data_list,"flt_data",headings_list,data_path)
         #todo add a second write; a telemetry csv, need to pop the values not wanted for transmission
         data_values = []  # remove the values now stored in the csv files
         time.sleep(delay)
+        first_pass = False
         print('-------------\n')
         print('\n')
-         
+
     #todo write the values to a csv file in a partucular directory
